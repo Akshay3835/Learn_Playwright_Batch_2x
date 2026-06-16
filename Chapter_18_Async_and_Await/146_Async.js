0a042ddda9acaacd3a8f7e906cd0d63f16260380
Akshay3835 <akshaykumarhindanavees@gmail.com>
2026-06-16 15:56:37 +0530

    feat: add Chapter 17 Promise files (Promise.all, interview Q&A) and Chapter 18 Async/Await, update README

---
+++ Chapter_18_Async_and_Await/146_Async.js
@@ -0,0 +1,16 @@
+async function testAPI() {
+    try {
+        let result = await Promise.reject( "503 reject" );
+    }
+    catch ( error ) {
+        console.log( "Error", error );
+
+    }
+    finally {
+        console.log( " Clean UP!" );
+
+    }
+
+}
+
+testAPI();
